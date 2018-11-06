91b1cb455a05a30700b9c3dba47927205500c30f
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-11-06 10:18:43 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,31 @@
-//define functions here
+function getIt(){
+$('p').on("click", function(){
+    alert ("Hey!");
+});
+}
+
+function frameIt(){
+  $(document).on("load", function(){
+    $('img').addClass("tasty")
+  });
+}
+
+function pressIt(){
+  $(document).on('keydown', function(key) {
+  if(key.which == 71){
+      alert('G was pressed');
+   }
+  });
+}
+
+function submitIt(){
+  $(document).on("submit", function() {
+    alert ("Your form is going to be submitted now.")
+  });   
+}
+
+
+
 
 $(document).ready(function(){
 
